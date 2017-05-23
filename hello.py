5fd004b20f96758cd628a72727150aa98bbf3bde
NoWMH <almazon-95@yandex.ru>
2017-05-23 16:22:08 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@ def app(environ, start_response):
 	#BUSINNES, MAT EGO, LOGIC, EEE
 	bodypozitiv = '\n'.join(environ['QUERY_STRING'].split('&'))
 	status = '200 OK'
-	headers = [ ('Content-Type', 'text/plain') ]
+	headers = [ ('Content-Type', 'text/plain'),
+        ('Content-Length', str(len(data)))]
 	start_response(status, headers)
 	return iter([ bodypozitiv ])
 	
