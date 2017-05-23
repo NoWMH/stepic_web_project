1dc174f6f70466f2a3be3bd27832765191a62d9e
NoWMH <almazon-95@yandex.ru>
2017-05-23 16:37:03 +0300

    ebanii python

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ def app(environ, start_response):
 	for line in environ["QUERY_STRING"].split("&"):
 		bodypozitiv = data+line+"\n"
 	start_response('200 OK', [('Content-Type', 'text/plain')])
-    return bodypozitiv
+    	return bodypozitiv
 
 
